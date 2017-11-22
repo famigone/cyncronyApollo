600b9ae80ad3d8c6cfb2a866c8d3d8edc32fc8f4
fede.amigone@gmail.com <fede.amigone@gmail.com>
2017-11-22 11:57:07 -0300

    asdfasdf

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,41 +4,12 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
 export const Tasks = new Mongo.Collection('tasks');
 
-/*if (Meteor.isServer) {
-  // This code only runs on the server
-  Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find();
-  });
-}*/
-
 // Deny all client-side updates since we will be using methods to manage this collection
 Tasks.deny({
   insert() { return false ; },
   update() { return true; },
   remove() { return true; },
 });
-
-
-/*Tasks.schema = new SimpleSchema({
-  nombre: { type: String },
-  inicio: { type: Date },
-  duracion: { type: Number },
-  //projectId: { type: String, regEx: SimpleSchema.RegEx.Id },
-  avance: { type: Number, decimal: true, defaultValue: 0, optional: true},
-  parentId: { type: Number, optional: true }, //padre
-  activo: { type: Boolean, defaultValue: true }, //borrado lógico
-  orden: { type: Number }, //id interno del componente
-  createdBy: {
-        type: String,
-        optional: true,
-        autoValue: function(){ return this.userId }
-    },
-  createdAt: {
-        type: Date,
-        optional: true,
-        autoValue: function(){ return new Date() }
-    },
-}); */
 
 
 Tasks.schema = new SimpleSchema({
