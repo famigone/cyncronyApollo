592ca84a492aeb0121ffd9c489380b214ec736b5
fede <fede.amigone@gmail.com>
2017-11-23 22:30:59 -0300

    asdf

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -16,6 +16,7 @@ import ProjectList from '../../ui/components/ProjectList/ProjectList';
 import ProjectNew from '../../ui/components/ProjectNew/ProjectNew';
 import ProjectInsert from '../../ui/components/ProjectNew/ProjectInsert';
 import App from '../../ui/components/Gantt/App';
+import Board from '../../ui/components/Boards/Board';
 
 
 export const requireAuth = (nextState, replace) => {
@@ -40,6 +41,7 @@ export const Routes = () => (
         <Route path="projectnew" name="projectnew" component={ProjectNew} />
         <Route path="projectinsert" name="projectinsert" component={ProjectInsert} />
         <Route path="tasks" name="tasks" component={ListaTarea} />
+        <Route path="boards/:id" name="boards" component={Board} />
 
       </Route>
     </Route>
---
+++ imports/ui/components/Boards/Board.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// Task component - represents a single todo item
+export default class Board extends Component {
+  render() {
+    return (
+      <tr>
+      <td>dddd</td>
+      <td>lala</td>
+    </tr>
+
+    );
+  }
+}
+
